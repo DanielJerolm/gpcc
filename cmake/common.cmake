e605b0c08b86b8de69f3733089ae0abd24695e31
Daniel Jerolm <daniel.jerolm@web.de>
2022-10-12 18:57:37 +0200

    Added some missing license notes.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,3 +1,12 @@
+# General Purpose Class Collection (GPCC)
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2022 Daniel Jerolm
+
+
 function(Validate_GPCC_Compiler)
   # This checks that the used compiler matches the user setting in "GPCC_Compiler".
 
