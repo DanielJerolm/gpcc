12e9fc103b8f70ad15d464867adce80b875548aa
Daniel Jerolm <daniel.jerolm@web.de>
2024-07-12 12:46:54 +0200

    Update copyright notices

--- include/gpcc/osal/os/linux_x64_tfc/Thread.hpp
+++ include/gpcc/osal/os/linux_x64_tfc/Thread.hpp
@@ -5,7 +5,7 @@
     If a copy of the MPL was not distributed with this file,
     You can obtain one at https://mozilla.org/MPL/2.0/.
 
-    Copyright (C) 2011 Daniel Jerolm
+    Copyright (C) 2011, 2024 Daniel Jerolm
 */
 
 #ifdef OS_LINUX_X64_TFC
--- src/execution/async/SuspendableDWQwithThread.cpp
+++ src/execution/async/SuspendableDWQwithThread.cpp
@@ -5,7 +5,7 @@
     If a copy of the MPL was not distributed with this file,
     You can obtain one at https://mozilla.org/MPL/2.0/.
 
-    Copyright (C) 2023 Daniel Jerolm
+    Copyright (C) 2023, 2024 Daniel Jerolm
 */
 
 #include <gpcc/execution/async/SuspendableDWQwithThread.hpp>
