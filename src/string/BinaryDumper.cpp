6b0bdf5c785651f19e66c0f46ecb3541f63bfed9
Daniel Jerolm <daniel.jerolm@web.de>
2025-01-10 14:28:01 +0100

    Add class BinaryDumper

---
+++ src/string/BinaryDumper.cpp
@@ -0,0 +1,359 @@
+/*
+    General Purpose Class Collection (GPCC)
+
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+    If a copy of the MPL was not distributed with this file,
+    You can obtain one at https://mozilla.org/MPL/2.0/.
+
+    Copyright (C) 2025 Daniel Jerolm
+*/
+
+#include <gpcc/string/BinaryDumper.hpp>
+#include <gpcc/string/StringComposer.hpp>
+#include <gpcc/string/tools.hpp>
+#include <gpcc/compiler/builtins.hpp>
+#include <stdexcept>
+
+namespace gpcc   {
+namespace string {
+
+/**
+ * \brief Constructor.
+ *
+ * - - -
+ *
+ * __Exception safety:__\n
+ * Strong guarantee.
+ *
+ * __Thread cancellation safety:__\n
+ * No cancellation point included.
+ *
+ * - - -
+ *
+ * \param address
+ * Address associated with the first word in the buffer referenced by @p pData. \n
+ * This is used for printing only. No memory access to the given address will occur.
+ *
+ * \param pData
+ * Pointer to the data that shall be dumped.\n
+ * `nullptr` is not allowed. This must be aligend to @p wordSize.
+ *
+ * \param nBytes
+ * Size of the data referenced by @p pData in bytes.\n
+ * Zero is allowed. This must be an integer multiple of @p wordSize.
+ *
+ * \param wordSize
+ * Word size (in byte) that shall be used to interpret the data referenced by @p pData. \n
+ * Allowed values: 1, 2, 4, or 8.
+ */
+BinaryDumper::BinaryDumper(uintptr_t const address,
+                           void const * const pData,
+                           size_t const nBytes,
+                           uint8_t const wordSize)
+: address_(address)
+, pData_(pData)
+, nWords_(0U)
+, wordSize_(wordSize)
+, wordsPerLine_(0U)
+, addressFieldWidth_(0U)
+, maxLineLength_(0U)
+{
+  // ---------------------------------------------------
+  // Check parameters
+  // ---------------------------------------------------
+  if (pData_ == nullptr)
+    throw std::invalid_argument("pData");
+
+  if ((wordSize_ != 1U) && (wordSize_ != 2U) && (wordSize_ != 4U) && (wordSize_ != 8U))
+    throw std::invalid_argument("wordSize");
+
+  if ((reinterpret_cast<uintptr_t>(pData_) % wordSize_) != 0U)
+    throw std::invalid_argument("pData <-> wordSize");
+
+  if ((nBytes % wordSize_) != 0U)
+    throw std::invalid_argument("nBytes <-> wordSize");
+
+  nWords_ = nBytes / wordSize_;
+  wordsPerLine_ = bytesPerLine_ / wordSize_;
+
+  // ---------------------------------------------------
+  // Calculate required field width for the address
+  // ---------------------------------------------------
+  // determine the address printed in the last line of the dump
+  uint_fast8_t const bytesInIncompleteLastLine = (nBytes % bytesPerLine_);
+  bool const lastLineFull = (bytesInIncompleteLastLine == 0U);
+
+  size_t addressSummand = nBytes - bytesInIncompleteLastLine;
+  if ((addressSummand != 0U) && (lastLineFull))
+    addressSummand -= bytesPerLine_;
+
+  uintptr_t lastPrintedAddr;
+  if (gpcc::compiler::OverflowAwareAdd(address_, addressSummand, &lastPrintedAddr))
+    throw std::invalid_argument("address overflow");
+
+  // determine the number of bits required to represent the address printed in the last line of the dump
+  uint_fast8_t nbOfRequiredBits = (sizeof(uintptr_t) * 8U) - gpcc::compiler::CountLeadingZeros(lastPrintedAddr);
+  if (nbOfRequiredBits < 8U)
+    nbOfRequiredBits = 8U;
+
+  // convert to bytes and round up to next byte boundary
+  uint_fast8_t const nbOfRequiredBytes = (nbOfRequiredBits + 7U) / 8U;
+
+  // determine the required number of hex digits
+  uint_fast8_t const nbOfRequiredHexDigits = nbOfRequiredBytes * 2U;
+
+  // determine the field width required to print the address in front of each line
+  addressFieldWidth_ = 2U + nbOfRequiredHexDigits + 2U;
+  //                   |    |                       |
+  //                   0x   hex digits              trailing ": "
+
+  // the headline ("Address ") requires a minimum width of 8
+  if (addressFieldWidth_ < 8U)
+    addressFieldWidth_ = 8U;
+
+  // ---------------------------------------------------
+  // Calculate the maximum length of each generated line
+  // ---------------------------------------------------
+  //  8 bit: Address XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX | 0123456789ABCDEF
+  // 16 bit: Address XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX | 0123456789ABCDEF
+  // 32 bit: Address XXXXXXXX XXXXXXXX XXXXXXXX XXXXXXXX | 0123456789ABCDEF
+  // 64 bit: Address XXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXX | 0123456789ABCDEF
+  maxLineLength_ = static_cast<uint8_t>(addressFieldWidth_ + (wordsPerLine_ * ((wordSize_ * 2U) + 1U)) + 18U);
+}
+
+/**
+ * \brief Retrieves a headline for the dump.
+ *
+ * - - -
+ *
+ * __Thread safety:__\n
+ * The state of the object is not modified. Concurrent accesses are safe.
+ *
+ * __Exception safety:__\n
+ * Strong guarantee.
+ *
+ * __Thread cancellation safety:__\n
+ * No cancellation point included.
+ *
+ * - - -
+ *
+ * \return
+ * String containing the headline for the dump.
+ */
+std::string BinaryDumper::GetHeadLine(void) const
+{
+  StringComposer sc(maxLineLength_);
+  sc << StringComposer::AlignLeft << StringComposer::Width(addressFieldWidth_) << "Address ";
+
+  sc << StringComposer::BaseHex << StringComposer::Uppercase;
+  for (uint_fast8_t i = 0U; i < wordsPerLine_; ++i)
+  {
+    sc << '+' << StringComposer::Width(wordSize_ * 2U) << static_cast<unsigned int>(i * wordSize_);
+  }
+
+  sc << "| 0123456789ABCDEF";
+
+  return sc.Get();
+}
+
+/**
+ * \brief Retrieves one line of dumped data.
+ *
+ * If all data has been dumped (or if there was no data), then the line will only contain the address of the next word
+ * that would be dumped if there was any data.
+ *
+ * - - -
+ *
+ * __Thread safety:__\n
+ * The state of the object is modified. Any concurrent accesses are not safe.
+ *
+ * __Exception safety:__\n
+ * Basic guarantee:
+ * - The object is left in a valid, but undefined state.
+ *
+ * __Thread cancellation safety:__\n
+ * No cancellation point included.
+ *
+ * - - -
+ *
+ * \return
+ * String containing one line of dumped data.
+ */
+std::string BinaryDumper::GetLine(void)
+{
+  StringComposer sc(maxLineLength_);
+  sc << StringComposer::AlignRightPadZero << StringComposer::BaseHex << StringComposer::Uppercase;
+
+  // dump the address
+  sc << "0x" << StringComposer::Width(addressFieldWidth_ - 4U) << address_ << ": ";
+
+  // dump the data, if any
+  if (nWords_ != 0U)
+  {
+    uint_fast8_t const wordsInThisLine = (nWords_ > wordsPerLine_) ? wordsPerLine_ : nWords_;
+    uint_fast8_t const bytesInThisLine = wordsInThisLine * wordSize_;
+
+    // Note: AppendHexDump() will pad the line with whitespaces if wordsInThisLine is less than wordsPerLine_.
+    AppendHexDump(sc, reinterpret_cast<uint8_t const*>(pData_), wordsInThisLine);
+    sc << "| ";
+    AppendASCIIDump(sc, reinterpret_cast<char const *>(pData_), bytesInThisLine);
+
+    // update pointer, address, and remaining words
+    pData_ = reinterpret_cast<uint8_t const*>(pData_) + bytesInThisLine;
+    nWords_ -= wordsInThisLine;
+    address_ += bytesPerLine_;
+  }
+
+  return sc.Get();
+}
+
+/**
+ * \brief Queries, if all data has been dumped.
+ *
+ * - - -
+ *
+ * __Thread safety:__\n
+ * The state of the object is not modified. Concurrent accesses are safe.
+ *
+ * __Exception safety:__\n
+ * No-throw guarantee.
+ *
+ * __Thread cancellation safety:__\n
+ * No cancellation point included.
+ *
+ * - - -
+ *
+ * \retval true   All data has been dumped or there was no data.
+ * \retval false  There is at least one word left to be dumped.
+ */
+bool BinaryDumper::IsAllDataDumped(void) noexcept
+{
+  return (nWords_ == 0U);
+}
+
+/**
+ * \brief Dumps 16 bytes of data into an @ref StringComposer using hexadecimal format.
+ *
+ * The dump always contains a trailing white-space.
+ *
+ * The dump will be padded with white-spaces if @p nbOfWords is less than @ref wordsPerLine_. The resulting length of
+ * the dump is always the same as if @ref wordsPerLine_ words of data had been dumped.
+ *
+ * - - -
+ *
+ * __Thread safety:__\n
+ * The state of the object is not modified. Concurrent accesses are safe.
+ *
+ * __Exception safety:__\n
+ * Strong guarantee.
+ *
+ * __Thread cancellation safety:__\n
+ * No cancellation point included.
+ *
+ * - - -
+ *
+ * \param sc
+ * The data will be dumped into the referenced @ref StringComposer. \n
+ * The @ref StringComposer must be configured as follows:\n
+ * AlignRightPadZero, BaseHex, UpperCase
+ *
+ * \param pData
+ * Pointer to the data that shall be dumped.\n
+ * This a pointer to `uint8_t`, but the data will be read and interpreted according to @ref wordSize_.
+ *
+ * \param nbOfWords
+ * Number of words that shall be read from @p pData and dumped to @p sc.
+ */
+void BinaryDumper::AppendHexDump(StringComposer & sc, uint8_t const * pData, uint_fast8_t const nbOfWords) const
+{
+  for (uint_fast8_t i = 0U; i < wordsPerLine_; ++i)
+  {
+    sc << StringComposer::Width(wordSize_ * 2U);
+
+    if (i < nbOfWords)
+    {
+      switch (wordSize_)
+      {
+        case 1U:
+        {
+          uint16_t const v = *reinterpret_cast<uint8_t const*>(pData);
+          sc << v;
+          break;
+        }
+
+        case 2U:
+        {
+          uint16_t const v = *reinterpret_cast<uint16_t const*>(pData);
+          sc << v;
+          break;
+        }
+
+        case 4U:
+        {
+          uint32_t const v = *reinterpret_cast<uint32_t const*>(pData);
+          sc << v;
+          break;
+        }
+
+        case 8U:
+        {
+          uint64_t const v = *reinterpret_cast<uint64_t const*>(pData);
+          sc << v;
+          break;
+        }
+      }
+
+      pData += wordSize_;
+    }
+    else
+    {
+      sc << ' '; // note: Will be padded with white spaces until "wordSize_ * 2U"
+    }
+
+    sc << ' ';
+  }
+}
+
+/**
+ * \brief Dumps 16 bytes of data into an @ref StringComposer using ASCII characters.
+ *
+ * Non-printable characters in the data will be substituted with "." characters.
+ *
+ * - - -
+ *
+ * __Thread safety:__\n
+ * The state of the object is not modified. Concurrent accesses are safe.
+ *
+ * __Exception safety:__\n
+ * Strong guarantee.
+ *
+ * __Thread cancellation safety:__\n
+ * No cancellation point included.
+ *
+ * - - -
+ *
+ * \param sc
+ * The data will be dumped into the referenced @ref StringComposer.
+ *
+ * \param pData
+ * Pointer to the data that shall be dumped.
+ *
+ * \param nbOfBytes
+ * Number of bytes that shall be read from @p pData and dumped to @p sc.
+ */
+void BinaryDumper::AppendASCIIDump(StringComposer & sc, char const * pData, uint_fast8_t nbOfBytes) const
+{
+  while (nbOfBytes != 0U)
+  {
+    char c = *pData;
+    if (!IsPrintableASCII(c))
+      c = '.';
+    sc << c;
+
+    ++pData;
+    --nbOfBytes;
+  }
+}
+
+} // namespace string
+} // namespace gpcc
---
+++ testcases/string/TestBinaryDumper.cpp
@@ -0,0 +1,551 @@
+/*
+    General Purpose Class Collection (GPCC)
+
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+    If a copy of the MPL was not distributed with this file,
+    You can obtain one at https://mozilla.org/MPL/2.0/.
+
+    Copyright (C) 2025 Daniel Jerolm
+*/
+
+#include <gpcc/string/BinaryDumper.hpp>
+#include <gpcc/compiler/definitions.hpp>
+#include <gtest/gtest.h>
+#include <stdexcept>
+
+static_assert(GPCC_SYSTEMS_ENDIAN == GPCC_LITTLE, "Tests dumping U16, U32, U64 will fail due to wrong byte order");
+
+namespace gpcc_tests {
+namespace string     {
+
+TEST(gpcc_string_BinaryDumper, CTOR_DataPtrInvalid)
+{
+  // pV is aligned to 64 bit
+  uint64_t v = 0;
+  uint8_t const * pV = reinterpret_cast<uint8_t const*>(&v);
+
+  ASSERT_NO_THROW(gpcc::string::BinaryDumper uut(0x10UL, pV, 8U, 8U));
+
+  // test nullptr
+  EXPECT_THROW(gpcc::string::BinaryDumper uut(0x10UL, nullptr, 0U, 1U), std::invalid_argument);
+
+  // test invalid alignment
+  EXPECT_THROW(gpcc::string::BinaryDumper uut(0x10UL, pV + 1U, 0U, 2U), std::invalid_argument);
+  EXPECT_THROW(gpcc::string::BinaryDumper uut(0x10UL, pV + 1U, 0U, 4U), std::invalid_argument);
+  EXPECT_THROW(gpcc::string::BinaryDumper uut(0x10UL, pV + 1U, 0U, 8U), std::invalid_argument);
+}
+
+TEST(gpcc_string_BinaryDumper, CTOR_WordSizeInvalid)
+{
+  // pV is aligned to 64 bit
+  uint64_t v = 0;
+  uint8_t const * pV = reinterpret_cast<uint8_t const*>(&v);
+
+  ASSERT_NO_THROW(gpcc::string::BinaryDumper uut(0x10UL, pV, 8U, 8U));
+
+  // test invalid values
+  EXPECT_THROW(gpcc::string::BinaryDumper uut(0x10UL, pV, 0U, 3U), std::invalid_argument);
+  EXPECT_THROW(gpcc::string::BinaryDumper uut(0x10UL, pV, 0U, 9U), std::invalid_argument);
+}
+
+TEST(gpcc_string_BinaryDumper, CTOR_nBytesInvalid)
+{
+  // pV is aligned to 64 bit
+  uint64_t v[2] = {0, 0};
+  uint8_t const * pV = reinterpret_cast<uint8_t const*>(v);
+
+  ASSERT_NO_THROW(gpcc::string::BinaryDumper uut(0x10UL, pV, 8U, 8U));
+
+  // test invalid alignment
+  EXPECT_THROW(gpcc::string::BinaryDumper uut(0x10UL, v, 3U, 2U), std::invalid_argument);
+  EXPECT_THROW(gpcc::string::BinaryDumper uut(0x10UL, v, 6U, 4U), std::invalid_argument);
+  EXPECT_THROW(gpcc::string::BinaryDumper uut(0x10UL, v, 9U, 8U), std::invalid_argument);
+}
+
+TEST(gpcc_string_BinaryDumper, Headlines_1ByteAddress)
+{
+  uint64_t const data[1] =
+  {
+    0x00
+  };
+
+  {
+    gpcc::string::BinaryDumper uut(0x10UL, data, 0U, 1U);
+    auto const s = uut.GetHeadLine();
+    EXPECT_STREQ(s.c_str(), "Address +0 +1 +2 +3 +4 +5 +6 +7 +8 +9 +A +B +C +D +E +F | 0123456789ABCDEF");
+    //                       0x0010: xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx | xxxxxxxxxxxxxxxx
+  }
+
+  {
+    gpcc::string::BinaryDumper uut(0x10UL, data, 0U, 2U);
+    auto const s = uut.GetHeadLine();
+    EXPECT_STREQ(s.c_str(), "Address +0   +2   +4   +6   +8   +A   +C   +E   | 0123456789ABCDEF");
+    //                       0x0010: xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx | xxxxxxxxxxxxxxxx
+  }
+
+  {
+    gpcc::string::BinaryDumper uut(0x10UL, data, 0U, 4U);
+    auto const s = uut.GetHeadLine();
+    EXPECT_STREQ(s.c_str(), "Address +0       +4       +8       +C       | 0123456789ABCDEF");
+    //                       0x0010: xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx | xxxxxxxxxxxxxxxx
+  }
+
+  {
+    gpcc::string::BinaryDumper uut(0x10UL, data, 0U, 8U);
+    auto const s = uut.GetHeadLine();
+    EXPECT_STREQ(s.c_str(), "Address +0               +8               | 0123456789ABCDEF");
+    //                       0x0010: xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxx | xxxxxxxxxxxxxxxx
+  }
+}
+
+TEST(gpcc_string_BinaryDumper, Headlines_4ByteAddress)
+{
+  uint64_t const data[1] =
+  {
+    0x00
+  };
+
+  {
+    gpcc::string::BinaryDumper uut(0x10000000UL, data, 0U, 1U);
+    auto const s = uut.GetHeadLine();
+    EXPECT_STREQ(s.c_str(), "Address     +0 +1 +2 +3 +4 +5 +6 +7 +8 +9 +A +B +C +D +E +F | 0123456789ABCDEF");
+    //                       0x10000000: xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx | xxxxxxxxxxxxxxxx
+  }
+
+  {
+    gpcc::string::BinaryDumper uut(0x10000000UL, data, 0U, 2U);
+    auto const s = uut.GetHeadLine();
+    EXPECT_STREQ(s.c_str(), "Address     +0   +2   +4   +6   +8   +A   +C   +E   | 0123456789ABCDEF");
+    //                       0x10000000: xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx | xxxxxxxxxxxxxxxx
+  }
+
+  {
+    gpcc::string::BinaryDumper uut(0x10000000UL, data, 0U, 4U);
+    auto const s = uut.GetHeadLine();
+    EXPECT_STREQ(s.c_str(), "Address     +0       +4       +8       +C       | 0123456789ABCDEF");
+    //                       0x10000000: xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx | xxxxxxxxxxxxxxxx
+  }
+
+  {
+    gpcc::string::BinaryDumper uut(0x10000000UL, data, 0U, 8U);
+    auto const s = uut.GetHeadLine();
+    EXPECT_STREQ(s.c_str(), "Address     +0               +8               | 0123456789ABCDEF");
+    //                       0x10000000: xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxx | xxxxxxxxxxxxxxxx
+  }
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U8_0Bytes)
+{
+  uint8_t const data[1] =
+  {
+    0xFFU
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, 0U, 1U);
+  std::string s;
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U8_8Bytes_1ByteAddress)
+{
+  uint8_t const data[8] =
+  {
+    0x41U, 0x42U, 0x61U, 0xFFU, 0xABU, 0x21U, 0x7EU, 0x12U
+  };
+
+  gpcc::string::BinaryDumper uut(0x10UL, data, sizeof(data), 1U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x0010: 41 42 61 FF AB 21 7E 12                         | ABa..!~.");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x0020: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x0020: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U8_8Bytes_4ByteAddress)
+{
+  uint8_t const data[8] =
+  {
+    0x41U, 0x42U, 0x61U, 0xFFU, 0xABU, 0x21U, 0x7EU, 0x12U
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 1U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: 41 42 61 FF AB 21 7E 12                         | ABa..!~.");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U8_16Bytes)
+{
+  uint8_t const data[16] =
+  {
+    0x41U, 0x42U, 0x61U, 0xFFU, 0xABU, 0x21U, 0x7EU, 0x12U, 0x30U, 0x31U, 0x32U, 0x33U, 0x34U, 0x35U, 0x36U, 0x37U
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 1U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: 41 42 61 FF AB 21 7E 12 30 31 32 33 34 35 36 37 | ABa..!~.01234567");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U8_20Bytes)
+{
+  uint8_t const data[20] =
+  {
+    0x41U, 0x42U, 0x61U, 0xFFU, 0xABU, 0x21U, 0x7EU, 0x12U, 0x30U, 0x31U, 0x32U, 0x33U, 0x34U, 0x35U, 0x36U, 0x37U,
+    0x54U, 0x65U, 0x73U, 0x74U
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 1U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: 41 42 61 FF AB 21 7E 12 30 31 32 33 34 35 36 37 | ABa..!~.01234567");
+
+  EXPECT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: 54 65 73 74                                     | Test");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000020: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U16_0Bytes)
+{
+  uint16_t const data[1] =
+  {
+    0xFFFFU
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, 0U, 2U);
+  std::string s;
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U16_8Bytes)
+{
+  uint16_t const data[4] =
+  {
+    0x4241U, 0xFF61U, 0x21ABU, 0x127EU
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 2U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: 4241 FF61 21AB 127E                     | ABa..!~.");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U16_16Bytes)
+{
+  uint16_t const data[8] =
+  {
+    0x4241U, 0xFF61U, 0x21ABU, 0x127EU, 0x3130U, 0x3332U, 0x3534U, 0x3736U
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 2U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: 4241 FF61 21AB 127E 3130 3332 3534 3736 | ABa..!~.01234567");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U16_20Bytes)
+{
+  uint16_t const data[10] =
+  {
+    0x4241U, 0xFF61U, 0x21ABU, 0x127EU, 0x3130U, 0x3332U, 0x3534U, 0x3736U,
+    0x6554U, 0x7473U
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 2U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: 4241 FF61 21AB 127E 3130 3332 3534 3736 | ABa..!~.01234567");
+
+  EXPECT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: 6554 7473                               | Test");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000020: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U32_0Bytes)
+{
+  uint32_t const data[1] =
+  {
+    0xFFFFFFFFUL
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, 0U, 4U);
+  std::string s;
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U32_8Bytes)
+{
+  uint32_t const data[2] =
+  {
+    0xFF614241UL, 0x127E21ABUL
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 4U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: FF614241 127E21AB                   | ABa..!~.");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U32_16Bytes)
+{
+  uint32_t const data[4] =
+  {
+    0xFF614241UL, 0x127E21ABUL, 0x33323130UL, 0x37363534UL
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 4U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: FF614241 127E21AB 33323130 37363534 | ABa..!~.01234567");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U32_20Bytes)
+{
+  uint32_t const data[5] =
+  {
+    0xFF614241UL, 0x127E21ABUL, 0x33323130UL, 0x37363534UL,
+    0x74736554UL
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 4U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: FF614241 127E21AB 33323130 37363534 | ABa..!~.01234567");
+
+  EXPECT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: 74736554                            | Test");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000020: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U64_0Bytes)
+{
+  uint64_t const data[1] =
+  {
+    0xFFFFFFFFFFFFFFFFULL
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, 0U, 8U);
+  std::string s;
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U64_8Bytes)
+{
+  uint64_t const data[1] =
+  {
+    0x127E21ABFF614241ULL
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 8U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: 127E21ABFF614241                  | ABa..!~.");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U64_16Bytes)
+{
+  uint64_t const data[2] =
+  {
+    0x127E21ABFF614241ULL, 0x3736353433323130ULL
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 8U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: 127E21ABFF614241 3736353433323130 | ABa..!~.01234567");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+TEST(gpcc_string_BinaryDumper, Dump_U64_24Bytes)
+{
+  uint64_t const data[3] =
+  {
+    0x127E21ABFF614241ULL, 0x3736353433323130ULL,
+    0x0000000074736554ULL
+  };
+
+  gpcc::string::BinaryDumper uut(0x10000000UL, data, sizeof(data), 8U);
+  std::string s;
+
+  ASSERT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000000: 127E21ABFF614241 3736353433323130 | ABa..!~.01234567");
+
+  EXPECT_FALSE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000010: 0000000074736554                  | Test....");
+
+  EXPECT_TRUE(uut.IsAllDataDumped());
+  s = uut.GetLine();
+  EXPECT_STREQ(s.c_str(), "0x10000020: ");
+
+  ASSERT_TRUE(uut.IsAllDataDumped());
+}
+
+} // namespace string
+} // namespace gpcc_tests
