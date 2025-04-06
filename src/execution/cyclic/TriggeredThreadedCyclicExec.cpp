2ebe1ba0d544a1ceb41682945e4b4685f0d45c07
Daniel Jerolm <daniel.jerolm@web.de>
2025-04-06 16:51:58 +0200

    TriggeredThreadedCyclicExec: Refactor doxygen and code

--- src/execution/cyclic/TriggeredThreadedCyclicExec.cpp
+++ src/execution/cyclic/TriggeredThreadedCyclicExec.cpp
@@ -25,33 +25,33 @@ namespace cyclic {
  * After creation of the object, @ref StartThread() must be called to start the object's thread. After that,
  * cyclic sampling can be started and stopped via @ref RequestStartSampling() and @ref RequestStopSampling().
  *
- * ---
+ * - - -
  *
  * __Exception safety:__\n
  * Strong guarantee.
  *
  * __Thread cancellation safety:__\n
- * Deferred cancellation is safe.
+ * Strong guarantee.
  *
- * ---
+ * - - -
  *
  * \param pThreadName
- * Pointer to a null-terminated c-string with the name for the object's thread.\n
- * _No copy is generated._\n
- * _The referenced string must not change during lifetime of the TriggeredThreadedCyclicExec object._\n
- * _nullptr is not allowed._
+ * Name for the object's thread. This must point to a null-terminated string.
+ * `nullptr` is not allowed.
+ *
  * \param trigger
- * Reference to an [IIRQ2ThreadWakeup](@ref gpcc::stdif::IIRQ2ThreadWakeup) subclass instance that shall be
- * used to deliver the cyclic trigger.
+ * Reference to an [IIRQ2ThreadWakeup](@ref gpcc::stdif::IIRQ2ThreadWakeup) interface that shall be used to deliver the
+ * cyclic trigger.
+ *
  * \param timeout
- * Reference to a [TimeSpan](@ref gpcc::time::TimeSpan) instance providing the timeout for monitoring the cyclic
- * trigger. This should be approximately the expected period plus a reasonable safety-margin.\n
- * _A copy is generated._
+ * Timeout for monitoring the cyclic trigger.\n
+ * This should be approximately the expected period plus a reasonable safety-margin.
+ *
  * \param isPllLockedFunc
- * Functor to a function/method that shall be used to retrieve if the PLL driving `_trigger` is in the locked
- * state or not.\n
- * If no PLL is used to drive `_trigger`, or if the lock state shall not be monitored, then no function/method
- * must be referenced. For details please refer to the documentation of @ref tIsPllLocked. \n
+ * Functor to a function/method that shall be used to query if the PLL driving @p trigger is in the locked state or
+ * not.\n
+ * If no PLL is used to drive @p trigger, or if the lock state shall not be monitored, then no function/method
+ * shall be referenced. For details please refer to the documentation of @ref tIsPllLocked. \n
  * _A copy is generated._
  */
 TriggeredThreadedCyclicExec::TriggeredThreadedCyclicExec(char const * const pThreadName,
@@ -65,84 +65,90 @@ TriggeredThreadedCyclicExec::TriggeredThreadedCyclicExec(char const * const pThr
 , mutex_()
 , asyncReqFlags_(static_cast<uint8_t>(AsyncReqFlags::none))
 , state_(States::stopped)
-, startDelayCnt_(0)
+, startDelayCnt_(0U)
 {
 }
 
 /**
  * \brief Destructor.
  *
- * The following must be ensured:
- * - The object must be in state @ref States::stopped
- * - the object's thread must be terminated (call to @ref StopThread())
+ * \pre   The object must be in state @ref States::stopped.
  *
- * ---
+ * \pre   The object's thread must be terminated (call to @ref StopThread()).
+ *
+ * - - -
  *
  * __Exception safety:__\n
  * No-throw guarantee.
  *
  * __Thread cancellation safety:__\n
- * Safe, no cancellation point included.
+ * No cancellation point included.
  */
 TriggeredThreadedCyclicExec::~TriggeredThreadedCyclicExec(void)
 {
   gpcc::osal::MutexLocker mutexLocker(mutex_);
   if (state_ != States::stopped)
-    osal::Panic("TriggeredThreadedCyclicExec::~TriggeredThreadedCyclicExec: Still running");
+    PANIC();
 }
 
 /**
  * \brief Retrieves a null-terminated c-string with the name of an @ref States value.
  *
+ * - - -
+ *
  * __Thread safety:__\n
  * This is thread-safe.
  *
  * __Exception safety:__\n
- * Strong guarantee.
+ * No-throw guarantee.
  *
  * __Thread cancellation safety:__\n
- * Safe, no cancellation point included.
+ * No cancellation point included.
  *
- * ---
+ * - - -
  *
  * \param state
  * @ref States value for which a null-terminated c-string with the value's name shall be retrieved.
+ *
  * \return
- * Pointer to a null-terminated c-string with the name of the @ref States value specified by parameter 'state'.
+ * Pointer to a null-terminated c-string with the name of the @ref States value specified by @p state.
  */
-char const * TriggeredThreadedCyclicExec::State2String(States const state)
+char const * TriggeredThreadedCyclicExec::State2String(States const state) noexcept
 {
   switch (state)
   {
-    case States::stopped:   return "stopped";
-    case States::starting:     return "start";
-    case States::waitLock:  return "waitLock";
-    case States::running:       return "run";
-    default:
-      throw std::invalid_argument("TriggeredThreadedCyclicExec::State2String: unknown state");
+    case States::stopped:  return "stopped";
+    case States::starting: return "start";
+    case States::waitLock: return "waitLock";
+    case States::running:  return "run";
   } // switch (state)
+
+  PANIC();
 }
 
 /**
  * \brief Retrieves a null-terminated c-string with the name of an @ref StopReasons value.
  *
+ * - - -
+ *
  * __Thread safety:__\n
  * This is thread-safe.
  *
  * __Exception safety:__\n
- * Strong guarantee.
+ * No-throw guarantee.
  *
  * __Thread cancellation safety:__\n
- * Safe, no cancellation point included.
+ * No cancellation point included.
  *
- * ---
+ * - - -
  *
  * \param code
  * @ref StopReasons value for which a null-terminated c-string with the value's name shall be retrieved.
+ *
  * \return
- * Pointer to a null-terminated c-string with a the name of the @ref StopReasons value specified by parameter 'code'.
+ * Pointer to a null-terminated c-string with a the name of the @ref StopReasons value specified by @p code.
  */
-char const * TriggeredThreadedCyclicExec::StopReasons2String(StopReasons const code)
+char const * TriggeredThreadedCyclicExec::StopReasons2String(StopReasons const code) noexcept
 {
   switch (code)
   {
@@ -151,32 +157,34 @@ char const * TriggeredThreadedCyclicExec::StopReasons2String(StopReasons const c
     case StopReasons::triggerTimeout:  return "triggerTimeout";
     case StopReasons::pllLossOfLock:   return "pllLossOfLock";
     case StopReasons::sampleRetFalse:  return "sampleRetFalse";
-    default:
-      throw std::invalid_argument("TriggeredThreadedCyclicExec::StopReasons2String: unknown code");
   }
+
+  PANIC();
 }
 
 /**
  * \brief Retrieves a textual description for an @ref StopReasons value.
  *
+ * - - -
+ *
  * __Thread safety:__\n
  * This is thread-safe.
  *
  * __Exception safety:__\n
- * Strong guarantee.
+ * No-throw guarantee.
  *
  * __Thread cancellation safety:__\n
- * Safe, no cancellation point included.
+ * No cancellation point included.
  *
- * ---
+ * - - -
  *
  * \param code
  * @ref StopReasons value for which a textual description shall be retrieved.
+ *
  * \return
- * Pointer to a null-terminated c-string with a textual description of the @ref StopReasons value
- * specified by parameter 'code'.
+ * Pointer to a null-terminated c-string with a textual description of the @ref StopReasons value specified by @p code.
  */
-char const * TriggeredThreadedCyclicExec::StopReasons2Description(StopReasons const code)
+char const * TriggeredThreadedCyclicExec::StopReasons2Description(StopReasons const code) noexcept
 {
   switch (code)
   {
@@ -185,18 +193,17 @@ char const * TriggeredThreadedCyclicExec::StopReasons2Description(StopReasons co
     case StopReasons::triggerTimeout:  return "Trigger timeout";
     case StopReasons::pllLossOfLock:   return "PLL loss of lock";
     case StopReasons::sampleRetFalse:  return "Sample() returned false";
-    default:
-      throw std::invalid_argument("TriggeredThreadedCyclicExec::StopReasons2String: unknown code");
   }
+
+  PANIC();
 }
 
 /**
  * \brief Starts the object's thread. This does not yet start sampling.
  *
- * The thread must not yet be running.\n
- * After calling this, sampling can be enabled by calling @ref RequestStartSampling() if not yet done.
+ * \pre   The thread must not yet be running.
  *
- * ---
+ * - - -
  *
  * __Thread safety:__\n
  * This is thread-safe.
@@ -205,17 +212,19 @@ char const * TriggeredThreadedCyclicExec::StopReasons2Description(StopReasons co
  * Strong guarantee.
  *
  * __Thread cancellation safety:__\n
- * Deferred cancellation is safe.
+ * Strong guarantee.
  *
- * ---
+ * - - -
  *
  * \param schedPolicy
  * Scheduling policy that shall be used for the new thread. See @ref gpcc::osal::Thread::SchedPolicy for details.
+ *
  * \param priority
  * Priority level: 0 (low) .. 31 (high)\n
  * This is only relevant for the scheduling policies @ref gpcc::osal::Thread::SchedPolicy::Fifo and
  * @ref gpcc::osal::Thread::SchedPolicy::RR. \n
  * _For the other scheduling policies this must be zero._
+ *
  * \param stackSize
  * Size of the stack of the new thread in byte.\n
  * _This must be a multiple of `gpcc::osal::Thread::GetStackAlign()`._\n
@@ -230,17 +239,11 @@ void TriggeredThreadedCyclicExec::StartThread(osal::Thread::SchedPolicy const sc
 }
 
 /**
- * \brief Cancels the object's thread and waits until the thread has terminated and joined.
+ * \brief Stops the object's thread and waits until the thread has terminated and joined.
  *
- * Note:\n
- * Sampling is not stopped gracefully. Instead the object's thread is cancelled using deferred cancellation.
+ * \pre   The thread is running.
  *
- * The thread must not yet be stopped.
- *
- * This will block until the object's thread has terminated and has been cleaned-up (joined).\n
- * After this has returned, it is safe to restart the object's thread via @ref StartThread() or to destroy the object.
- *
- * ---
+ * - - -
  *
  * __Thread safety:__\n
  * This is thread-safe.
@@ -253,40 +256,36 @@ void TriggeredThreadedCyclicExec::StartThread(osal::Thread::SchedPolicy const sc
  */
 void TriggeredThreadedCyclicExec::StopThread(void) noexcept
 {
-  try
-  {
-    thread_.Cancel();
-    (void)thread_.Join();
-  }
-  catch (...)
-  {
-    PANIC();
-  }
+  thread_.Cancel();
+  (void)thread_.Join();
 }
 
 /**
  * \brief Requests start of sampling.
  *
- * The current state must be @ref States::stopped and there must be no pending start or stop request.\n
- * It is not mandatory, that the object's thread is running.
+ * It is not mandatory, that the object's thread is running. The thread may be started after calling this.
  *
- * ---
+ * \pre   The current state is @ref States::stopped.
+ *
+ * \pre   There is no pending start or stop request.
+ *
+ * - - -
  *
  * __Thread safety:__\n
  * This is thread-safe.\n
- * This may even be called in the context of the object's own thread.
+ * This may be called in the context of the object's own thread.
  *
  * __Exception safety:__\n
  * Strong guarantee.
  *
  * __Thread cancellation safety:__\n
- * Deferred cancellation is safe.
+ * No cancellation point included.
  *
- * ---
+ * - - -
  *
  * \param startDelay
  * Number of extra cycles the @ref TriggeredThreadedCyclicExec instance shall remain in state @ref States::starting
- * before moving to @ref States::waitLock. If this is zero, then one cycle is spend in state @ref States::starting.
+ * before switching to @ref States::waitLock. If this is zero, then one cycle is spend in state @ref States::starting.
  */
 void TriggeredThreadedCyclicExec::RequestStartSampling(uint8_t const startDelay)
 {
@@ -295,7 +294,7 @@ void TriggeredThreadedCyclicExec::RequestStartSampling(uint8_t const startDelay)
   if (state_ != States::stopped)
     throw std::logic_error("TriggeredThreadedCyclicExec::RequestStartSampling: Current state must be \"Stopped\"");
 
-  if ((asyncReqFlags_ & (static_cast<uint8_t>(AsyncReqFlags::start) | static_cast<uint8_t>(AsyncReqFlags::stop))) != 0)
+  if ((asyncReqFlags_ & (static_cast<uint8_t>(AsyncReqFlags::start) | static_cast<uint8_t>(AsyncReqFlags::stop))) != 0U)
     throw std::logic_error("TriggeredThreadedCyclicExec::RequestStartSampling: Start/Stop request already pending");
 
   asyncReqFlags_ |= static_cast<uint8_t>(AsyncReqFlags::start);
@@ -305,33 +304,36 @@ void TriggeredThreadedCyclicExec::RequestStartSampling(uint8_t const startDelay)
 /**
  * \brief Requests stop of sampling and removes a potential pending start request.
  *
- * There must be no pending stop request. Any pending start request is canceled.
+ * \pre   There must be no pending stop request.
  *
- * ---
+ * - - -
  *
  * __Thread safety:__\n
  * This is thread-safe.\n
- * This may even be called in the context of the object's own thread.
+ * This may be called in the context of the object's own thread.
  *
  * __Exception safety:__\n
  * Strong guarantee.
  *
  * __Thread cancellation safety:__\n
- * Deferred cancellation is safe.
+ * No cancellation point included.
  */
 void TriggeredThreadedCyclicExec::RequestStopSampling(void)
 {
   osal::MutexLocker mutexLocker(mutex_);
 
-  if ((asyncReqFlags_ & static_cast<uint8_t>(AsyncReqFlags::stop)) != 0)
+  if ((asyncReqFlags_ & static_cast<uint8_t>(AsyncReqFlags::stop)) != 0U)
     throw std::logic_error("TriggeredThreadedCyclicExec::RequestStopSampling: Stop request already pending");
 
   // set stop request flag and clear a potential start request flag
-  asyncReqFlags_ = (asyncReqFlags_ | static_cast<uint8_t>(AsyncReqFlags::stop)) & static_cast<uint8_t>(~static_cast<uint8_t>(AsyncReqFlags::start));
+  asyncReqFlags_ =   (asyncReqFlags_ | static_cast<uint8_t>(AsyncReqFlags::stop))
+                   & static_cast<uint8_t>(~static_cast<uint8_t>(AsyncReqFlags::start));
 }
 
 /**
  * \brief Retrieves the current state of the @ref TriggeredThreadedCyclicExec instance.
+ *
+ * - - -
  *
  * __Thread safety:__\n
  * This is thread-safe.
@@ -340,9 +342,9 @@ void TriggeredThreadedCyclicExec::RequestStopSampling(void)
  * Strong guarantee.
  *
  * __Thread cancellation safety:__\n
- * Deferred cancellation is safe.
+ * No cancellation point included.
  *
- * ---
+ * - - -
  *
  * \return
  * Current state of the @ref TriggeredThreadedCyclicExec instance.
@@ -356,6 +358,8 @@ TriggeredThreadedCyclicExec::States TriggeredThreadedCyclicExec::GetCurrentState
 /**
  * \brief Entry function for the object's thread.
  *
+ * - - -
+ *
  * __Thread safety:__\n
  * Not applicable. Program logic ensures that there can only be one thread at any time.
  *
@@ -363,9 +367,9 @@ TriggeredThreadedCyclicExec::States TriggeredThreadedCyclicExec::GetCurrentState
  * No-throw guarantee.
  *
  * __Thread cancellation safety:__\n
- * Deferred cancellation is safe.
+ * Strong guarantee.
  *
- * ---
+ * - - -
  *
  * \return
  * Always nullptr.
