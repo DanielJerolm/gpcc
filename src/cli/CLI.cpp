aef774a51e9bc7b9ac25906321b41d686c65877f
Daniel Jerolm <daniel.jerolm@wago.com>
2025-10-17 14:36:54 +0200

    fix(cli): Do not use strcmp
    This is a "hotfix". The bug will be fixes in the development branch.

--- src/cli/CLI.cpp
+++ src/cli/CLI.cpp
@@ -261,7 +261,7 @@ void CLI::AddCommand(std::unique_ptr<Command> spNewCmd)
     Command * pCurr = pCMDListHead;
     while (pCurr != nullptr)
     {
-      int const result = strcasecmp(spNewCmd->GetCommand(), pCurr->GetCommand());
+      int const result = strcmp(spNewCmd->GetCommand(), pCurr->GetCommand());
       if (result == 0)
       {
         throw std::logic_error("CLI::AddCommand: Command already registered");
