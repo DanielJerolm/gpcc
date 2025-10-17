c83ff13628eb7062fa5455bc6e1e422f81e30400
Daniel Jerolm <daniel.jerolm@wago.com>
2025-10-17 14:50:04 +0200

    fix: Link against proper Zephyr libs

--- CMakeLists.md
+++ CMakeLists.md
@@ -44,7 +44,7 @@ chibios_arm   | target_link_libraries(gpcc PUBLIC <chibios_and_cpp_runtime>)
 epos_arm      | Presence of library `epos_kernel`
 linux_arm     | -
 linux_x64     | -
-zephyr        | Presence of library `app`
+zephyr        | Presence of library `zephyr_interface`
 
 __Artifacts build:__
 - Static library `gpcc`
--- cmake/common.cmake
+++ cmake/common.cmake
@@ -277,7 +277,7 @@ function(SetupLinkLibraries target)
     target_link_libraries(${target} PUBLIC Threads::Threads PRIVATE rt)
 
   elseif(${GPCC_OS} MATCHES "zephyr")
-    target_link_libraries(${target} PUBLIC app)
+    target_link_libraries(${target} PUBLIC zephyr_interface)
 
   else()
     message(FATAL_ERROR "Error: Value of 'GPCC_OS' is not supported by function 'SetupLinkLibraries'.")
