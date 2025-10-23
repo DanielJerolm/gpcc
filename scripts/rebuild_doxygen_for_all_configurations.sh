0684f32af2f3cc41b45efc9da32af6f9e52a6280
Daniel Jerolm <daniel.jerolm@web.de>
2025-10-23 11:51:12 +0200

    fix style

--- scripts/rebuild_doxygen_for_all_configurations.sh
+++ scripts/rebuild_doxygen_for_all_configurations.sh
@@ -21,11 +21,11 @@
 set -e
 
 CONFIGURATIONS="chibios_arm chibios_arm_extract_all
-               epos_arm epos_arm_extract_all
-               linux_arm linux_arm_extract_all
-               linux_arm_tfc linux_arm_tfc_extract_all
-               linux_x64 linux_x64_extract_all
-               linux_x64_tfc linux_x64_tfc_extract_all"
+                epos_arm epos_arm_extract_all
+                linux_arm linux_arm_extract_all
+                linux_arm_tfc linux_arm_tfc_extract_all
+                linux_x64 linux_x64_extract_all
+                linux_x64_tfc linux_x64_tfc_extract_all"
 
 echo "Remove existing documentation"
 echo "================================================================================"
