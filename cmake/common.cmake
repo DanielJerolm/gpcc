22e066888942780922cf04f5921a0a5706bab57e
Daniel Jerolm <daniel.jerolm@wago.com>
2025-12-01 16:50:55 +0100

    Private linkage to "kernel" is sufficient

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -277,7 +277,7 @@ function(SetupLinkLibraries target)
     target_link_libraries(${target} PUBLIC Threads::Threads PRIVATE rt)
 
   elseif(${GPCC_OS} MATCHES "zephyr")
-    target_link_libraries(${target} PUBLIC zephyr_interface kernel)
+    target_link_libraries(${target} PUBLIC zephyr_interface PRIVATE kernel)
 
   else()
     message(FATAL_ERROR "Error: Value of 'GPCC_OS' is not supported by function 'SetupLinkLibraries'.")
